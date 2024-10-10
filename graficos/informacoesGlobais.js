a0d95ab1254987ac0ea98301e1edcd7a4709f8c0
naridienestadler <105284891+naridienestadler@users.noreply.github.com>
2024-10-10 07:45:23 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -9,6 +9,9 @@ async function visualizarInformacoesGlobais() {
         paragrafo.classList.add('graficos-container__texto')
         paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
         console.log(paragrafo);
+
+        const container = document.getElementById('graficos-container')
+  container.appendChild(paragrafo)
      
     }
 
